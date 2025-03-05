0510f635baaa5918513ca7aca6f7b0a5bef5b0b8
Arne Putzeys <arne.putzeys@student.kuleuven.be>
2025-03-05 22:03:26 +0000

    Revoking and restoring access via PTE

--- libsgxstep/pt_abstractions.c
+++ libsgxstep/pt_abstractions.c
@@ -3,35 +3,166 @@
 #include "debug.h"
 #include <sys/mman.h>
 #include "enclave.h"
+#include <stdint.h>
+#include <inttypes.h>
+#include <stdbool.h>
 
-#define MAX_PAGES 5000
-static uint64_t *pte_cache[MAX_PAGES] = {NULL}; // Not the most optimal datastructure, TODO: Change to map
+#define MAX_PAGES 6000
 
-int pte_removeperms(uint64_t *pte_pointer)
+typedef struct
+{
+    size_t start;
+    size_t end;
+
+} protected_range_t;
+
+// See screenshot on discord w.r.t segments for more information, possibly need to automate this
+// Information comes from /proc/self/maps; segments with just p permission, no RWX
+protected_range_t protected_ranges[] = {
+    {4258, 4274}, // 16 pages 0x7ffff50a2000 - 0x7ffff50b2000 (On specific screenshot/debugger attachment)
+    {4338, 4354}, // Past the end of the libjpeg pages, so should be fine
+};
+
+#define PROTECTED_RANGE_COUNT (sizeof(protected_ranges) / sizeof(protected_ranges[0]))
+
+static uint64_t *pte_cache[MAX_PAGES] = {NULL};
+static uint8_t shifted_pages[MAX_PAGES] = {0};
+
+/*
+This function is for internal state keeping
+
+It returns whether a PTE has had their PFN shifted
+True: shifted and thus invalid
+False: not shifted and thus valid
+*/
+static inline bool is_page_shifted(size_t pagenum)
+{
+    return shifted_pages[pagenum] != 0;
+}
+
+/*
+This function is for internal state keeping
+
+It marks if a PTE has had their PFN shifted,
+and thus is invalid
+*/
+static inline void mark_page_shifted(size_t pagenum)
+{
+    shifted_pages[pagenum] = 1;
+}
+
+/*
+This function is for internal state keeping
+
+It unmarks if a PTE has had their PFN unshifted,
+and thus is valid
+*/
+static inline void mark_page_unshifted(size_t pagenum)
+{
+    shifted_pages[pagenum] = 0;
+}
+
+/*
+This function is for internal state keeping
+
+It returns whether a certain segment is protected, meaning
+that it has no RWX permissions at all
+
+For example: guard pages
+*/
+int is_protected_segment(size_t page)
+{
+    for (size_t i = 0; i < PROTECTED_RANGE_COUNT; i++)
+    {
+        if (page >= protected_ranges[i].start && page <= protected_ranges[i].end)
+            return 1;
+    }
+    return 0;
+}
+
+// If the user bit is cleared and we write to a data page, no page fault in userspace is triggered
+// which results in the attack halting and not making any progress
+// Best guess: Happens due to kernel internals
+// This is why we are revoking access by changing the PFN
+
+// We revoke access by invalidating the physical address of the page, which sgx will throw a pagefault error on.
+int pte_removeperms_PFN(size_t pagenumber)
+{
+    if (is_page_shifted(pagenumber))
+    {
+        // If the access has already been revoked, do not shift further.
+        // Otherwise the internal state of this module is invalid.
+        return;
+    }
+    uint64_t *pte = get_pte_by_page(pagenumber);
+
+    if (!pte)
+        return; // Error retrieving PTE
+
+    uint64_t phys_addr = *pte & PT_PHYS_MASK;
+
+    // Add the offset (0x1000) to make invalid
+    uint64_t invalid_phys_addr = phys_addr + 0x1000;
+
+    // Preserve flags and update PTE
+    *pte = (*pte & ~PT_PHYS_MASK) | (invalid_phys_addr & PT_PHYS_MASK);
+
+    mark_page_shifted(pagenumber);
+}
+
+int pte_restoreperms_PFN(size_t pagenumber)
+{
+    if (!is_page_shifted(pagenumber))
+    {
+        return;
+    }
+    uint64_t *pte = get_pte_by_page(pagenumber);
+
+    if (!pte)
+        return; // Error retrieving PTE
+
+    uint64_t old_phys_addr = *pte & PT_PHYS_MASK;
+
+    // Subtract the offset to make valid again
+    uint64_t new_phys_addr = old_phys_addr - 0x1000;
+
+    // Preserve flags and update PTE
+    *pte = (*pte & ~PT_PHYS_MASK) | (new_phys_addr & PT_PHYS_MASK);
+    mark_page_unshifted(pagenumber);
+}
+
+// Not used currently
+int pte_removeperms_PRESENT(uint64_t *pte_pointer)
 {
     if (!PRESENT(*pte_pointer))
+    {
         return -1;
+    }
 
     *pte_pointer = MARK_SUPERVISOR(*pte_pointer);
     return 1;
 }
 
-int pte_restoreperms(uint64_t *pte_pointer)
+// Not used currently
+int pte_restoreperms_PRESENT(uint64_t *pte_pointer)
 {
     if (!PRESENT(*pte_pointer))
+    {
         return -1;
-
+    }
     *pte_pointer = MARK_USER(*pte_pointer);
     return 1;
 }
 
+// Converts a page number into the corresponding virtual address
 static inline void *pagenum_to_virt(size_t page)
 {
-    // Both are equivalent:
-    return (page << PT_SHIFT) + (size_t)get_enclave_base();
+    // Both implementations are equivalent:
+    return (void *)((page << PT_SHIFT) + (size_t)get_enclave_base());
     // return (size_t)get_enclave_base() + page * PAGE_SIZE_4KiB;
 }
 
+// Converts a virtual address into the corresponding page number
 static inline size_t virt_to_pagenum(void *virt)
 {
     return ((size_t)virt - (size_t)get_enclave_base()) >> PT_SHIFT;
@@ -41,42 +172,42 @@ uint64_t *get_pte_by_page(size_t page)
 {
     if (page >= MAX_PAGES)
     {
-        info("Out of bounds");
+        debug("Out of bounds of the page cache, increase MAX_PAGES in pt_abstractions.c");
         return NULL; // Out of bounds
     }
 
+    // If the page is in a protected segment, so no RWX permissions do not keep track of it
+    // Otherwise later in this function on the dereference step an error will occur.
+    if (is_protected_segment(page))
+    {
+        return NULL;
+    }
+
+    // If entry not in the cache, add it
     if (!pte_cache[page])
     {
-        info("PTE for page %d not in page cache yet, adding it", page);
         void *virt_addr = pagenum_to_virt(page);
-        info("Page %d translated to virtual address %p. Enclave Base: %p", page, virt_addr, get_enclave_base());
 
-        uint8_t buffer[4096];
-        info("Reading contents of page with edbgrd");
-
-        edbgrd(virt_addr, buffer, 4096);
-
-        // Print buffer in hex format
-        printf("Page contents:\n");
-        for (int j = 0; j < 128; j++)
+        if (!(get_enclave_base() < virt_addr < get_enclave_limit()))
         {
-            if (j % 16 == 0)
-                printf("\n");
-            printf("%02X ", buffer[j]);
+            debug("Page %lu out of range, not part of the enclave", page);
+            return NULL;
         }
-        printf("\n");
 
-        void *pte = remap_page_table_level(virt_addr, PTE);
-        info("PTE: %p for page %d", pte, page);
+        // Forcing the page to be present
+        volatile uint64_t force_page_to_be_present = *(uint64_t *)virt_addr;
 
-        print_page_table(virt_addr);
-        // info("Virt Addr: %p, PTE: %p", virt_addr, pte);
-        // print_pte_adrs(virt_addr);
-        //  print_pte(pte);
+        // mlock the page so it does not get unmapped
+        mlock(virt_addr, 4096);
 
+        // Remapping into user space
+        uint64_t *pte = remap_page_table_level(virt_addr, PTE);
+
+        // Adding it to the cache
         pte_cache[page] = pte;
     }
 
+    // Return the entry from the cache
     return pte_cache[page];
 }
 
@@ -90,50 +221,7 @@ int pte_revoke_pages(size_t page, size_t num_pages)
 {
     for (size_t i = 0; i < num_pages; i++)
     {
-        // int j = 0;
-        // edbgrd(pagenum_to_virt(page + i), &j, 4);
-        // int res = mlock(pagenum_to_virt(page + i), 4096);
-        // print_pte_adrs(pagenum_to_virt(page + i));
-        // if (res != 0)
-        // {
-        //     info("Cannot mlock");
-        // }
-
-        uint64_t *pte = get_pte_by_page(page + i);
-        if (!pte)
-        {
-            info("Error retrieving PTE");
-            return -1; // Error retrieving PTE
-        }
-
-        int result = pte_removeperms(pte);
-        if (result != 1)
-        {
-            info("Removing access to page %zu failed", page + i);
-            // print_pte_adrs(pagenum_to_virt(page + i));
-
-            // print_pte(get_pte_by_page(page + i));
-
-            // uint8_t buffer[4096];
-            // info("Reading contents of failed page with edbgrd");
-
-            // edbgrd(pagenum_to_virt(page + i), buffer, 4096);
-
-            // // Print buffer in hex format
-            // printf("Page contents:\n");
-            // for (int j = 0; j < 4096; j++)
-            // {
-            //     if (j % 16 == 0)
-            //         printf("\n");
-            //     printf("%02X ", buffer[j]);
-            // }
-            // printf("\n");
-
-            // print_pte(get_pte_by_page(page + i));
-
-            // // print_pte_adrs(pagenum_to_virt(page + i));
-            // info("Virtual page addrs %p", pagenum_to_virt(page + i));
-        }
+        pte_removeperms_PFN(page + i);
     }
     return 0; // Success
 }
@@ -142,19 +230,7 @@ int pte_restore_pages(size_t page, size_t num_pages)
 {
     for (size_t i = 0; i < num_pages; i++)
     {
-        uint64_t *pte = get_pte_by_page(page + i);
-        if (!pte)
-        {
-            info("Error retrieving PTE");
-            return -1; // Error retrieving PTE
-        }
-
-        int result = pte_restoreperms(pte);
-
-        if (result != 1)
-        {
-            info("Restoring access to page %zu failed", page + i);
-        }
+        pte_restoreperms_PFN(page + i);
     }
     return 0; // Success
 }
--- libsgxstep/pt_abstractions.h
+++ libsgxstep/pt_abstractions.h
@@ -4,16 +4,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-// Removes the permissions of a Page Table Entry
-// Concretely: Unsets the User bit.
-// Returns 1 if successful, -1 otherwise
-int pte_removeperms(uint64_t *pte_pointer);
-
-// Restores the permissions of a Page Table Entry
-// Concretely: Sets the User bit.
-// Returns 1 if successful, -1 otherwise
-int pte_restoreperms(uint64_t *pte_pointer);
-
 int pte_revoke_pages(size_t page, size_t num_pages);
 
 int pte_restore_pages(size_t page, size_t num_pages);
