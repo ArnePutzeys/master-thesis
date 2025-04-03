8f347befbf9c9e17f9317207bb34c6d0a134872b
Arne Putzeys <arne.putzeys@student.kuleuven.be>
2025-04-03 10:25:35 +0000

    Microbenchmark: Small change

--- app/microbenchmark/benchmark.sh
+++ app/microbenchmark/benchmark.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
-num_runs=100
+num_runs=1000
 
 
 for ((i=1; i<=num_runs; i++)); do
-    output_file="reconstruct_color_${image_name}_iteration_${i}.bmp"
     echo "---------------------------------------------------------------------------------------"
     echo "Iteration $i"
     echo "---------------------------------------------------------------------------------------"
-
     ./app
 
 done
